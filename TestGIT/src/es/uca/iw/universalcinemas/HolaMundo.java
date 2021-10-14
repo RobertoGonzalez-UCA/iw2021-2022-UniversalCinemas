332dfa129cb40cfde969924269e51de3341e5e99
RobertoGonzalez-UCA <roberto.gonzalezalvarez@alum.uca.es>
2021-10-14 10:38:14 +0200

    Añado otra linea mas

--- TestGIT/src/es/uca/iw/universalcinemas/HolaMundo.java
+++ TestGIT/src/es/uca/iw/universalcinemas/HolaMundo.java
@@ -15,6 +15,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
 		System.out.println("Creando conflicto!");
+		System.out.println("GAGADHSDASD");
 	}
 
 }
