bb0124e6ef3efedd737e7a42fd403297c052d68f
RobertoGonzalez-UCA <roberto.gonzalezalvarez@alum.uca.es>
2021-10-14 10:04:34 +0200

    Commit inicial usando Eclipse

---
+++ TestGIT/src/es/uca/iw/universalcinemas/HolaMundo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package es.uca.iw.universalcinemas;
+
+/**
+ * @author Personal
+ *
+ */
+public class HolaMundo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+	}
+
+}
