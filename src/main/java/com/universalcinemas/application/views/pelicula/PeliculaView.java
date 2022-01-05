4077ec8bc7a458911e13a3426034913a6f2b59e9
Rafa Rodríguez <rafa.rodricalve@alum.uca.es>
2022-01-05 09:36:50 +0100

    UI Choose Seat Done. Next step: develop business logic

--- src/main/java/com/universalcinemas/application/views/pelicula/PeliculaView.java
+++ src/main/java/com/universalcinemas/application/views/pelicula/PeliculaView.java
@@ -15,7 +15,9 @@ import com.universalcinemas.application.data.film.Film;
 import com.universalcinemas.application.data.film.FilmRepository;
 import com.universalcinemas.application.data.film.FilmService;
 import com.universalcinemas.application.data.session.Session;
+import com.universalcinemas.application.data.user.User;
 import com.universalcinemas.application.views.MainLayout;
+import com.vaadin.flow.component.HtmlComponent;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
@@ -26,6 +28,7 @@ import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
@@ -34,6 +37,9 @@ import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.server.ClassResource;
+import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.flow.component.html.Div;
 
 @PageTitle("Pelicula")
 @Route(value = "pelicula", layout = MainLayout.class)
@@ -100,6 +106,7 @@ public class PeliculaView extends VerticalLayout implements HasUrlParameter<Inte
 		Button elegirAsientoButton = new Button("Elegir asientos", e -> {
 			dialog.close();
 			Dialog dialog2 = new Dialog();
+			dialog2.setWidth("800px");
 			createDialogLayout2(dialog2);
 		});
 //		elegirAsientoButton.addClickListener(e -> 
@@ -154,36 +161,68 @@ public class PeliculaView extends VerticalLayout implements HasUrlParameter<Inte
 	private VerticalLayout createDialogLayout2(Dialog dialog) {
 		VerticalLayout verticalLayout = new VerticalLayout();
 		HorizontalLayout horizontalLayout = new HorizontalLayout();
-		H2 title = new H2("Elegir Asiento");
-		
-//		Grid<Map<String, Object>> grid = new Grid<>();
-//		String[] keys = {"id", "name", "dni", "avgsalary", "lastvisit"};
-//		Class<? extends PeliculaView> rows = getClass(); // return a list of Maps with keys like in the above list
-//		for (String key : keys) {
-//		  grid.addColumn("asd");
-//		}
-//		grid.addItems(rows);
-		
-    	verticalLayout.setWidth("90%");
-    	verticalLayout.setHeight("90%");
-    	horizontalLayout.setWidth("90%");
-    	horizontalLayout.setHeight("90%");
+		H2 title = new H2("ELEGIR ASIENTO");
+		Div divColor = new Div();
+		divColor.getElement().getStyle().set("background","linear-gradient(to bottom, red, black)");
+		divColor.getElement().getStyle().set("height","30px");
+		divColor.getElement().getStyle().set("margin-top", "10px");
+		divColor.getElement().getStyle().set("margin-bottom", "10px");
 
+
+		title.getElement().getStyle().set("text-align", "center");
+//		title.getElement().getStyle().set("margin-bottom", "30px");
+
+    	dialog.add(title,divColor);
+
+    	dialog.setMaxWidth("900px");
+    	dialog.setMaxHeight("700px");
+    	
+		for(int i = 0; i < 6; i++) {
+			HtmlComponent br = new HtmlComponent("br");
+			for (int j = 0; j < 12; j++) {
+				Image img2= new Image("https://p.kindpng.com/picc/s/111-1114911_person-icon-png-download-icono-usuario-png-transparent.png","User Image");
+				img2.setHeight("40px");
+				img2.setWidth("40px");
+				if(j == 0)
+					img2.getElement().getStyle().set("margin-left", "100px");
+				img2.getElement().getStyle().set("margin-right", "2px");
+
+				dialog.add(img2);
+				if(j == 3 || j == 7){
+					Div d = new Div();
+					d.getElement().getStyle().set("display", "inline");
+					d.getElement().getStyle().set("height", "700px");
+					d.getElement().getStyle().set("width", "2px");
+					d.getElement().getStyle().set("margin", "0 auto");
+					d.getElement().getStyle().set("padding", "0");
+					d.getElement().getStyle().set("border-left", "25px solid #000");
+					dialog.add(d);
+				}
+			}
+			dialog.add(br);
+		}
+		
+		
+		
     	horizontalLayout.add(verticalLayout);
-    	dialog.add(title);
+//    	dialog.add(title,img,img2,img3,img4);
 		dialog.open();
     	//add(title,horizontalLayout);
-    	
     	
     	setHeightFull();
 		setAlignItems(Alignment.CENTER);
         setJustifyContentMode(JustifyContentMode.CENTER);//puts button in vertical center
 		
-        
-		
-		
 		dialog.setCloseOnEsc(false);
 		dialog.setCloseOnOutsideClick(false);
+		
+		Button btnConfirmar = new Button("Confirmar");
+//		btnConfirmar.getElement().getStyle().set(null, null);
+		dialog.add(btnConfirmar);
+		btnConfirmar.addClickListener(e -> {
+ 			dialog.close(); 
+ 		});
+		
 		return verticalLayout;
 	}
 }
