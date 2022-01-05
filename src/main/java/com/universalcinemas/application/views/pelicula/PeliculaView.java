2857e7e6f871d077f622224901e709d864ee3d40
Rafa Rodríguez <rafa.rodricalve@alum.uca.es>
2022-01-05 06:34:15 +0100

    Developing Choose Seats View

--- src/main/java/com/universalcinemas/application/views/pelicula/PeliculaView.java
+++ src/main/java/com/universalcinemas/application/views/pelicula/PeliculaView.java
@@ -1,14 +1,12 @@
 package com.universalcinemas.application.views.pelicula;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Optional;
-
 import javax.annotation.security.PermitAll;
-
 import com.universalcinemas.application.data.business.Business;
 import com.universalcinemas.application.data.film.Film;
 import com.universalcinemas.application.data.film.FilmRepository;
@@ -19,18 +17,20 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
-import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-
 @PageTitle("Pelicula")
 @Route(value = "pelicula", layout = MainLayout.class)
 @PermitAll
@@ -64,7 +64,7 @@ public class PeliculaView extends VerticalLayout implements HasUrlParameter<Inte
     	Image img=new Image(film.get().getFilmPoster(), film.get().getFilmPoster());
         img.setWidth("350px");
         horizontalLayout.add(img);
-    	verticalLayout.add(new H1(film.get().getName()));
+    	verticalLayout.add(new Label(film.get().getName()));
     	verticalLayout.add(new Label(film.get().getSynopsis()));
     	verticalLayout.add(btn);
     	verticalLayout.setWidth("30%");
@@ -87,12 +87,25 @@ public class PeliculaView extends VerticalLayout implements HasUrlParameter<Inte
 //		DatePicker fechaPelicula = new DatePicker("Día");
 //		TimePicker horaPelicula = new TimePicker("Hora");
 //		IntegerField cantidadEntradas = new IntegerField();
-
 		Button cancelarCompraButton = new Button("Cancelar", e -> {
 			dialog.close();
 			sesionElegida = null;
 		});
-		Button elegirAsientoButton = new Button("Elegir asientos");
+		
+		Button elegirAsientoButton = new Button("Elegir asientos", e -> {
+			dialog.close();
+			Dialog dialog2 = new Dialog();
+			createDialogLayout2(dialog2);
+		});
+//		elegirAsientoButton.addClickListener(e -> 
+//		elegirAsientoButton.getUI().ifPresent(ui ->
+//	        ui.navigate("/chooseseat"))
+//		);
+		
+//		VerticalLayout dialogLayout2 = createDialogLayout(dialog);
+//		dialog.add(dialogLayout2);
+//		Button button22 = new Button("Show dialog", e -> dialog.open());
+//		add(dialog, button22);
 		
 		elegirAsientoButton.setEnabled(false);
 		elegirAsientoButton.setIcon(new Icon(VaadinIcon.ARROW_RIGHT));
@@ -132,4 +145,39 @@ public class PeliculaView extends VerticalLayout implements HasUrlParameter<Inte
 		
 		return dialogLayout;
     }
+	
+	private VerticalLayout createDialogLayout2(Dialog dialog) {
+		VerticalLayout verticalLayout = new VerticalLayout();
+		HorizontalLayout horizontalLayout = new HorizontalLayout();
+		H2 title = new H2("Elegir Asiento");
+		
+//		Grid<Map<String, Object>> grid = new Grid<>();
+//		String[] keys = {"id", "name", "dni", "avgsalary", "lastvisit"};
+//		Class<? extends PeliculaView> rows = getClass(); // return a list of Maps with keys like in the above list
+//		for (String key : keys) {
+//		  grid.addColumn("asd");
+//		}
+//		grid.addItems(rows);
+		
+    	verticalLayout.setWidth("90%");
+    	verticalLayout.setHeight("90%");
+    	horizontalLayout.setWidth("90%");
+    	horizontalLayout.setHeight("90%");
+    	horizontalLayout.add(verticalLayout);
+    	dialog.add(title);
+		dialog.open();
+    	//add(title,horizontalLayout);
+    	
+    	
+    	setHeightFull();
+		setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);//puts button in vertical center
+
+
+
+
+		dialog.setCloseOnEsc(false);
+		dialog.setCloseOnOutsideClick(false);
+		return verticalLayout;
+	}
 }
