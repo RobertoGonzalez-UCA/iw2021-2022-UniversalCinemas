3353d524ee7ff7937c8974c364ec747afd7eff29
Rafa Rodríguez <rafa.rodricalve@alum.uca.es>
2022-01-11 15:21:46 +0100

    fixing bugs in css styles within RegistroView

--- src/main/java/com/universalcinemas/application/views/registro/RegistroView.java
+++ src/main/java/com/universalcinemas/application/views/registro/RegistroView.java
@@ -2,9 +2,11 @@ package com.universalcinemas.application.views.registro;
 
 import com.universalcinemas.application.data.user.User;
 import com.universalcinemas.application.data.user.UserService;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.H3;
 import com.vaadin.flow.component.html.Paragraph;
@@ -13,6 +15,7 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Page;
 import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
@@ -33,13 +36,26 @@ public class RegistroView extends VerticalLayout {
         
 		this.userService = userService;
 				
+		Page page = UI.getCurrent().getPage();
+	    page.executeJs("document.getElementById('ROOT-2521314').id = 'root-signup';"
+	    			+ "document.getElementById('outlet').id = 'outlet-signup';"
+	    			+ "document.getElementsByTagName('vaadin-vertical-layout')[0].setAttribute('id', 'vaadin-vertical-layout-signup');"
+	    			+ "document.getElementsByTagName('body')[0].setAttribute('id', 'body-signup');");
+	    
 		H2 title = new H2("Universal Cinemas");
+		title.setId("h2-signup");
+		
 		Paragraph second_title = new Paragraph("Regístrate e inicia sesión para disfrutar de nuestra cartelera");
+		second_title.setId("p-signup");
+		
 		H3 third_title = new H3("Regístrese");
+		third_title.setId("h3-signup");
 
 		TextField name = new TextField("Nombre");
         TextField surname = new TextField("Apellidos");
+        
         HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setId("horizontalLayout-signup");
         horizontalLayout.add(name,surname);
         
         DatePicker birthDate = new DatePicker("Fecha nacimiento");
@@ -52,6 +68,7 @@ public class RegistroView extends VerticalLayout {
         emailField.setPattern("^(.+)@(.+)$");
         
         HorizontalLayout horizontalLayout2 = new HorizontalLayout();
+        horizontalLayout2.setId("horizontalLayout-signup");
         horizontalLayout2.add(birthDate,emailField);
         
         TextField phoneNumber = new TextField("Número de teléfono");
@@ -62,6 +79,7 @@ public class RegistroView extends VerticalLayout {
         horizontalLayout3.add(phoneNumber);
         
         HorizontalLayout horizontalLayout4 = new HorizontalLayout();
+        horizontalLayout4.setId("horizontalLayout-signup");
         horizontalLayout4.add(password1,password2);
         
         Button button = new Button("Registrarme", event -> register(
@@ -77,10 +95,10 @@ public class RegistroView extends VerticalLayout {
 
         
         HorizontalLayout horizontalLayout5 = new HorizontalLayout();
+        horizontalLayout5.setId("horizontalLayout-signup");
         Button button2 = new Button("Acceder a mi cuenta");
         button2.getElement().getStyle().set("background-color", "var(--lumo-primary-color)");
         horizontalLayout5.add(button,button2);
-
 
         add(title,second_title, third_title,horizontalLayout,horizontalLayout2,horizontalLayout3,horizontalLayout4,horizontalLayout5);
         
